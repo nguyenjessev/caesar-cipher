ef49e32cd03e36a98f29dd0ca84797f6ea033d00
Jesse <nguyenjessev@gmail.com>
2020-07-26 14:13:09 -0400

    Method now loops around, keeps case, and rejects non-letters

--- caesar.rb
+++ caesar.rb
@@ -1,10 +1,21 @@
 require 'pry'
 
 def caesar(text, offset)
-    result = ""
-    text.each_byte do |c|
-        result += (c + offset).chr
+  result = ""
+  text.each_byte do |c|
+    if (c >= 65 && c <= 90) || (c >= 97 && c <= 122)
+      if c < 97
+        range = 65
+      else
+        range = 97
+      end
+      newC = c + offset - range
+      newC %= 26
+      newC += range
+      result += newC.chr
+    else
+      result += c.chr
     end
-
-    return result
+  end
+  return result
 end
