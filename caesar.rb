7f9d558adf313f97d671706d18dab1d80f1fea6a
Jesse <nguyenjessev@gmail.com>
2020-08-29 12:01:43 -0400

    Clean up code

--- caesar.rb
+++ caesar.rb
@@ -1,21 +1,21 @@
-require 'pry'
+# frozen-string-literal: true
 
 def caesar(text, offset)
-  result = ""
+  result = ''
   text.each_byte do |c|
     if (c >= 65 && c <= 90) || (c >= 97 && c <= 122)
-      if c < 97
-        range = 65
-      else
-        range = 97
-      end
-      newC = c + offset - range
-      newC %= 26
-      newC += range
-      result += newC.chr
+      range = if c < 97
+                65
+              else
+                97
+              end
+      new_c = c + offset - range
+      new_c %= 26
+      new_c += range
+      result += new_c.chr
     else
       result += c.chr
     end
   end
-  return result
+  result
 end
